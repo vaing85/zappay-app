3a0d8e74917b0472eb1212206d57c8b2c9a334fd
vaing85 <mingdynasty35@protonmail.com>
2025-09-08 01:23:35 -0500

    Force rebuild to fix JavaScript file mismatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-// Force rebuild for Netlify static asset fix - v2
+// Force rebuild for Netlify static asset fix - v3
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
